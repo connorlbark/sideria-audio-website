21098842750991fa3b212c87f64586f18f9616a3
connorlbark <connorlbark@gmail.com>
2024-12-26 17:57:38 -0500

    under construction

---
+++ src/components/MatrixText.tsx
@@ -0,0 +1,116 @@
+import { useState, useEffect } from "react";
+
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+const generateRandomCharacter = (goal: string) => {
+  const charactersLength = characters.length;
+
+  const generate = () => {
+    return characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  let rand = generate();
+
+  while (rand === goal) {
+    rand = generate();
+  }
+
+  return rand;
+}
+
+const generateRandomCharArray = (length: number, goalArray: string[]) => {
+    let result = [];
+    for ( let i = 0; i < length; i++ ) {
+      result.push(generateRandomCharacter(goalArray[i]));
+    }
+
+    return result;
+}
+
+const computeGoalTextArray = (length: number, text: string) => {
+  const goal = []; 
+
+  for (let i = 0; i < length; i++) {
+    goal.push(text[i % text.length]);
+  }
+
+  return goal;
+
+}
+
+const copmuteRemainingIndices = (currentCharacters: string[], goalTextArray: string[]) => {
+  const remainingIndices = [];
+
+  for (let i = 0; i < currentCharacters.length; i++) {
+    if (currentCharacters[i].normalize() !== goalTextArray[i].normalize()) {
+      remainingIndices.push(i);
+    }
+  }
+  
+  return remainingIndices;
+}
+
+const updateText = (currentCharacters: string[], goalTextArray: string[], speed: number, glitchChance: number) => {
+  const remainingIndices = copmuteRemainingIndices(currentCharacters, goalTextArray);
+
+
+  const indicesToFix =  remainingIndices.sort(() => Math.random() - Math.random()).slice(0, Math.min(speed, remainingIndices.length))
+
+  
+  let chars = (currentCharacters.length !== 0 ? currentCharacters : generateRandomCharArray(goalTextArray.length, goalTextArray));
+
+  const output = goalTextArray.map((goalChar, i) => {
+
+    const glitch = Math.random() <= glitchChance;
+
+    if (!glitch && (indicesToFix.includes(i) || chars[i].normalize() === goalChar.normalize())) {
+      return goalTextArray[i];
+    } else {
+      return generateRandomCharacter(goalChar);
+    }
+  });
+
+  return output;
+}
+
+export const MatrixText = ({
+    text,
+    speed = 1,
+    glitchChance = 0.007,
+    className
+}: {
+    text: string,
+    speed?: number,
+    glitchChance?: number,
+    className?: string
+}) => {
+
+  const length = text.length
+
+  const goalTextArray: string[] = computeGoalTextArray(length, text);
+  const [currentCharacters, setCurrentCharacters] = useState<string[]>([]);
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    setCurrentCharacters(generateRandomCharArray(length, goalTextArray));
+    setInitialized(true)
+  }, [length, text])
+  
+  useEffect(() => {
+
+      const interval = setInterval(() => {
+        if (initialized) {
+          setCurrentCharacters(updateText(currentCharacters, goalTextArray, speed, glitchChance));
+        }
+  
+      }, 75);
+  
+    
+      return () => clearInterval(interval);
+  
+  }, [initialized, text, length, currentCharacters, speed]);
+
+  return <span className={className}>
+        {currentCharacters?.join('')}
+    </span>
+}
